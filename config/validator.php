8293b0a3f5bf5d5a2306af53a2fcfcc91e2b0526
Sergii Grebeniuk <grebenjuk.s@gmail.com>
2012-04-26 14:01:43 +0300

    simple Validator class

---
+++ config/validator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Validator config
+ *
+ * Copy to the application config directory and make your changes
+ */
+
+return array(
+
+    /**
+     * Required field
+     */
+    'required'  => array(
+        'rule'     => '\w+',
+        'message'  => '%name% field is required',
+    ),
+
+);
---
+++ validator.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Part of the Phrame
+ *
+ * @package    Core
+ * @version    0.4.0
+ * @author     Phrame Development Team
+ * @license    MIT License
+ * @copyright  2012 Phrame Development Team
+ * @link       http://phrame.itworks.in.ua/
+ */
+
+namespace Phrame\Core;
+
+/**
+ * Validator class
+ *
+ * @property  array  $errors  Validation errors
+ */
+class Validator
+{
+    /**
+     * Application name
+     *
+     * @var  string
+     */
+    protected $app_name;
+
+    /**
+     * Validation configuration
+     *
+     * @var  Config
+     */
+    protected $config;
+
+    /**
+     * Validation errors
+     *
+     * @var  array
+     */
+    public $errors = array();
+
+    /**
+     * Constructs Validation object
+     *
+     * @param  string  $app_name  Application name
+     */
+    public function __construct($app_name = null)
+    {
+        $this->app_name = $app_name ?: APPLICATION_NAME;
+        $this->config   = new Config('validator', $this->app_name);
+    }
+
+    /**
+     * Validates value on rule
+     *
+     * @param   mixed   $value       Value
+     * @param   string  $rule_name   Rule name
+     * @param   array   $parameters  Field name and other parameters
+     * @param   string  $message     Message
+     * @return  bool
+     */
+    public function validate($value, $rule_name, $parameters = array(), $message = '')
+    {
+        $valid = preg_match('#'.$this->config[$rule_name]['rule'].'#', $value) > 0;
+
+        if ( ! $valid)
+        {
+            if ( ! is_array($parameters))
+            {
+                $parameters = array('name' => $parameters);
+            }
+
+            $keys = array();
+            $values = array();
+            foreach ($parameters as $k => $v)
+            {
+                $keys[] = '%'.$k.'%';
+                $values[] = $v;
+            }
+
+            $message = $message ?: $this->config[$rule_name]['message'];
+            $this->errors[] = str_replace($keys, $values, $message);
+        }
+
+        return $valid;
+    }
+
+}
